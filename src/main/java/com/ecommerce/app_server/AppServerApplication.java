9513119c72361d57c329a40b75e8091deaeb351f
Alkesh Gupta <thealkeshgupta@gmail.com>
2025-06-09 20:55:44 +0530

    Initial Commit - Initial Project Setup

---
+++ src/main/java/com/ecommerce/app_server/AppServerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.app_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppServerApplication.class, args);
+	}
+
+}
