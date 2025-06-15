f84fcd0c2313f465a47debebda662195018c92f0
Alkesh Gupta <thealkeshgupta@gmail.com>
2025-06-15 23:44:08 +0530

    User Authentication Implementation - added User Model and Repository - added Role Model and Repository - added custom UserDetails and UserDetailsService - added Web Security Configuration - added JWT Util, auth entry point and token filter - added Cookie based Authentication

---
+++ src/main/java/com/ecommerce/app_server/model/AppRole.java
@@ -0,0 +1,7 @@
+package com.ecommerce.app_server.model;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_SELLER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/ecommerce/app_server/security/response/MessageResponse.java
@@ -0,0 +1,10 @@
+package com.ecommerce.app_server.security.response;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
